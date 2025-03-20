2ee5f66d7370e260d6d0bab17ee6ec11f8623275
Ndubuaku Chigozie <victoriousman711@gmail.com>
2025-03-20 09:15:05 +0100

    Initial Commit

---
+++ src/assets/assets.js
@@ -0,0 +1,10 @@
+import HeroL from "./Hero-Left1.png";
+import HeroChildren from "./Hero-Children.png";
+
+
+ const assets = {
+    HeroL,
+    HeroChildren,
+}
+
+export default assets
---
+++ src/pages/home/HomePage.jsx
@@ -0,0 +1,9 @@
+function HomePage() {
+    return (
+        <div>
+            <p className="color-primary">Hello World</p>
+        </div>
+    )
+}
+
+export default HomePage
