a9d847d8c94ff300e7d87e9cdb2e2a5ff367eda1
Ndubuaku Chigozie <victoriousman711@gmail.com>
2025-03-22 00:53:56 +0100

    New Features added

--- src/pages/home/ShowcaseSection.jsx
+++ src/pages/home/ShowcaseSection.jsx
@@ -1,6 +1,6 @@
 function ShowcaseSection() {
     return (
-        <section className="bg-secondary h-[100vh]">
+        <section className=" h-[100vh]">
             Welcome to Show case, Here you will see great wonders. 
         </section>
     )
